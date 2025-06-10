b795e9951b9890931c110b21d782d1218c9c58a1
AdrianBustanil <rianbustanil450@gmail.com>
2025-06-10 15:53:02 +0700

    try

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -6,5 +6,14 @@ using namespace std;
 int main()
 {
     cout << "Awal Program" << endl;
-    
+    try {
+        array<int, 3> data = { 10, 20, 30 };
+        cout<<data.at(5)<<endl;
+    }
+    catch (exception& e) {
+        cout << e.what() << endl;
+    }
+    cout << "Baris Program Yang terakhir" << endl;
+
+    return 0;
 }
