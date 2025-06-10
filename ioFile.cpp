b58509b859eba119890d7d0fea3a42265fa8e9ec
AdrianBustanil <rianbustanil450@gmail.com>
2025-06-10 15:28:31 +0700

    infile else cout

--- ioFile.cpp
+++ ioFile.cpp
@@ -40,6 +40,10 @@ int main() {
             //dan tampilkan disini
             cout << baris << '\n';
         }
-
+        //tutup file tersebut setelah selesai 
+        infile.close();
     }
+    //jika tidak ditemukan file maka akan menampilkan ini 
+    else cout << "unable to open file";
+    return 0;
 }
