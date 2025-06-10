3d9b597de0f05b5d087254a509068a37b8f98574
AdrianBustanil <rianbustanil450@gmail.com>
2025-06-10 15:50:26 +0700

    outfile close

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -20,13 +20,14 @@ int main () {
         if (baris == "q") break;
         outfile << baris << endl;
     }
-    
+
     
     outfile.close();
     ifstream infile;
     infile.open(NamaFile, ios::in);
     cout << endl << ">= Membuka dan membaca file " << endl;
     if (infile.is_open())
+    
     {
         while (getline(infile, baris))
         {
